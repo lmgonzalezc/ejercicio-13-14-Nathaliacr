be67cea3c215878a2832aff79907a1857fbf0939
Mauricio González <46940813+lmgonzalezc@users.noreply.github.com>
2019-05-15 19:05:35 -0500

    Ejercicio cpp V2

--- Ejercicio.cpp
+++ Ejercicio.cpp
@@ -5,21 +5,27 @@ using namespace std;
 const double w0 =1.0 , gam =0.2;
 const double h =0.1 , delta =1.0;
 const double F =0.4 , w =0.2;
-const int N = 1000;
-
+const int N =1000;
 ofstream outfile;
-
 double g( double theta0 , double omega0 , double t);
 double f( double theta0 , double omega0 , double t);
-
 int main ( void ){
-    double omega =0.1;
-    double theta =0;
+    
+    double omega =1;
+    double theta =0.0;
     outfile.open("datos.dat");
     for ( int i =0; i < N;i ++){
-        outfile<< i*h << " " << theta << " " << omega << " \n " ;
-        omega = omega + h* f(theta , omega , i*h);
-        theta = theta + h* g(theta , omega , i*h);
+        outfile<< i*h << " " << theta << " " << omega << "\n";
+        omega = omega +h* f(theta , omega , i*h);
+        theta = theta +h* g(theta , omega , i*h);
+    }
+    outfile.close();
+    double theta2 =0.01;
+    outfile.open("datos2.dat");
+    for ( int i =0; i < N;i ++){
+        outfile<< i*h << " " << theta2 << " " << omega << "\n";
+        omega = omega +h* f(theta2 , omega , i*h);
+        theta2 = theta2 +h* g(theta2 , omega , i*h);
     }
     outfile.close();
     return 0;
